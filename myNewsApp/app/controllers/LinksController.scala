d50af978a61f1c298811d24c18229d17b0715f74
OstroS <ostros@gmail.com>
2014-06-16 22:17:55 +0200

    ...

--- myNewsApp/app/controllers/LinksController.scala
+++ myNewsApp/app/controllers/LinksController.scala
@@ -3,6 +3,12 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+/**
+ * Purpose of this controller is to:
+ * - present information about stored links,
+ * - add links by user,
+ * - deliver possibility to search among stored links.
+ */
 object LinksController extends Controller {
   
   def index = Action {
