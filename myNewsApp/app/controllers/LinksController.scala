40eb08890ab20f900ff5b939b87a0fc8dffe898e
OstroS <ostros@gmail.com>
2014-07-07 23:31:25 +0200

    Link mapping

--- myNewsApp/app/controllers/LinksController.scala
+++ myNewsApp/app/controllers/LinksController.scala
@@ -17,14 +17,16 @@ object LinksController extends Controller {
   def index = Action {
     Ok(views.html.link(Link.getLastNElements(10), urlForm))
   }
-  
 
+  def create = Action { implicit request =>
+    Ok("vals=" + urlForm.bindFromRequest.get)
+  }
 
-  def create = Action { implicit request => 
-    Ok("request: " + request)
-    }
-  
-  
   val urlForm = Form(
-	"url" -> text)
+    mapping(
+      "url" -> text,
+      "description" -> text)
+      ((url, description) => Link(999, url, description, List("tag")))
+      ((link: Link) => Some((link.url, link.description))))
+  	    
 }
